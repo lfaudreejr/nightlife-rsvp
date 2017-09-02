dfe3d8d7c1f1447e1f17c648814d6c53c289f410
Larry Faudree <lfaudreejr@hotmail.com>
2017-09-01 21:12:09 -0400

    fixing angular

--- src/app/auth/auth-config.ts
+++ src/app/auth/auth-config.ts
@@ -1,11 +1,11 @@
-import { ENV } from '../core/env.config'
+import { ENV } from '../core/env.config';
 
 interface AuthConfig {
-  CLIENT_ID: string
-  CLIENT_DOMAIN: string
-  AUDIENCE: string
-  REDIRECT: string
-  SCOPE: string
+  CLIENT_ID: string;
+  CLIENT_DOMAIN: string;
+  AUDIENCE: string;
+  REDIRECT: string;
+  SCOPE: string;
 }
 
 export const AUTH_CONFIG: AuthConfig = {
